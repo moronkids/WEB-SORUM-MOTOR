e58a6690bf1283293e692e1017456be74b990b13
moronkids <moronkids@gmail.com>
2019-02-03 16:35:42 +0700

    fix update - image update form, function update

--- app/Http/Controllers/MotorController.php
+++ app/Http/Controllers/MotorController.php
@@ -3,16 +3,17 @@ namespace App\Http\Controllers;
 use App\Motor;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\File;
-
-
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class MotorController extends Controller
 {
+
     public function index(){
       $Motor = Motor::all();
       return view('web_extends.shop',['Motor' => $Motor]);
     }
+
     public function create()
     {
         return view('web_extends.create_shop');
@@ -53,11 +54,17 @@ class MotorController extends Controller
         'brand_motor'=> 'required',
         'tipe_motor' => 'required'
       ]);
+      $gmabarmotor = $request->file('gambarmotor');
+      $extension = $gmabarmotor->getClientOriginalExtension();
+      Storage::disk('public')->put($gmabarmotor->getFilename().'.'.$extension,  File::get($gmabarmotor));
 
           $motors = Motor::find($id);
           $motors->nama_motor = $request->get('nama_motor');
           $motors->brand_motor = $request->get('brand_motor');
           $motors->tipe_motor = $request->get('tipe_motor');
+          $motors->mime = $gmabarmotor->getClientMimeType();
+          $motors->original_filename = $gmabarmotor->getClientOriginalName();
+          $motors->filename = $gmabarmotor->getFilename().'.'.$extension;
           $motors->save();
 
           return redirect('/Motors')->with('success', 'Stock has been updated');
